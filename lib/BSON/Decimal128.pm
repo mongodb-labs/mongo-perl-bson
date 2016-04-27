5d431edb666731c7af0cd1376a8dfb0fe04dd39c
David Golden <xdg@xdg.me>
2016-04-27 12:52:18 -0400

    Improve code/comments for Decimal128

--- lib/BSON/Decimal128.pm
+++ lib/BSON/Decimal128.pm
@@ -12,6 +12,15 @@ use Math::BigInt;
 
 use Class::Tiny qw/value/;
 
+use constant {
+    PLIM  => 34,    # precision limit, i.e. max coefficient chars
+    EMAX  => 6144,  # for 9.999999999999999999999999999999999E+6144
+    EMIN  => -6143, # for 1.000000000000000000000000000000000E-6143
+    AEMAX => 6111,  # EMAX - (PLIM - 1); largest encodable exponent
+    AEMIN => -6176, # EMIN - (PLIM - 1); smallest encodable exponent
+    BIAS  => 6176,  # offset for encoding exponents
+};
+
 sub new_from_bytes {
     my ($class, $bid) = @_;
     return $class->new( defined($bid) ? ( value => _bid_to_string($bid) ) : () );
@@ -28,21 +37,24 @@ my $strict_re = qr{
   |  (?: -? Inf )                                  # infinities
   |  (?: -? [0-9]{1,34} )                          # integer form
   |  (?: -? 0\.[0-9]{1,6} )                        # short decimal form
-  |  (?: -? [0-9]\.[0-9]{1,33} E [+-] $digits )    # exponential form
+  |  (?: -? [0-9]\.[0-9]{1,33} E ([+-] $digits) )  # exponential form
 }x;
 
 sub BUILD {
     my $self = shift;
     $self->{value} = "" unless defined $self->{value};
 
-    # maybe normalize representation
-    # XXX need to range check here, too
-    return if $self->{value} =~ /\A $strict_re \z/x;
+    # skip normalization if already in standar form
+    if ( $self->{value} =~ /\A $strict_re \z/x ) {
+        # but if $1 has a value, check that it's in range
+        return if !$1 || ( $1 >= EMIN && $1 <= EMAX );
+    };
     $self->{value} = _bid_to_string( _string_to_bid( $self->{value} ) );
 }
 
 sub bytes {
     my $self = shift;
+    no warnings 'once';
     return _string_to_bid($self->{value}) if $BSON::Types::NoCache;
     return $self->{_bytes} if defined $self->{_bytes};
     return $self->{_bytes} = _string_to_bid($self->{value});
@@ -69,16 +81,16 @@ sub _bid_to_string {
         # Bits: 1*sign 2*ignored 14*exponent 111*significand.
         # Implicit 0b100 prefix in significand.
         $coef = "" . Math::BigInt->new( "0b100" . substr( $binary, 17 ) );
-        $e = unpack( "n", pack( "B*", "00" . substr( $binary, 3, 14 ) ) ) - 6176;
+        $e = unpack( "n", pack( "B*", "00" . substr( $binary, 3, 14 ) ) ) - BIAS;
     }
     else {
         # Bits: 1*sign 14*exponent 113*significand
         $coef = "" . Math::BigInt->new( "0b" . substr( $binary, 15 ) );
-        $e = unpack( "n", pack( "B*", "00" . substr( $binary, 1, 14 ) ) ) - 6176;
+        $e = unpack( "n", pack( "B*", "00" . substr( $binary, 1, 14 ) ) ) - BIAS;
     }
 
     # Out of range is treated as zero
-    if ( length($coef) > 34 ) {
+    if ( length($coef) > PLIM ) {
         $coef = "0";
     }
 
@@ -87,6 +99,7 @@ sub _bid_to_string {
         return $pos ? "0" : "-0";
     }
 
+    # convert to scientific form ( e.g. 123E+4 -> 1.23E6 )
     my $adj_exp = $e + length($coef) - 1;
 
     # exponential notation
@@ -100,9 +113,9 @@ sub _bid_to_string {
             ( $pos ? "" : "-" ) . $coef . "E" . ( $adj_exp >= 0 ? "+" : "" ) . $adj_exp );
     }
 
-    # not exponential notation (small negative exponents)
+    # not exponential notation (integers or small negative exponents)
     else {
-        # maybe no exponent? (i.e. positive/negative integer)
+        # e == 0 means integer
         return $pos ? $coef : "-$coef"
           if $e == 0;
 
@@ -115,8 +128,8 @@ sub _bid_to_string {
         return $pos ? "0.$coef" : "-0.$coef"
           if length($coef) == abs($e);
 
-        # otherwise, decimal goes in the middle
-        substr( $coef, -abs($e), 0, "." );
+        # otherwise length(coef) > abs($e), so insert dot after first digit
+        substr( $coef, 1, 0, "." );
         return $pos ? $coef : "-$coef";
     }
 }
@@ -125,6 +138,8 @@ my ( $bidNaN, $bidPosInf, $bidNegInf ) =
   map { scalar reverse pack( "B*", $_ . ( "0" x 118 ) ) } qw/ 011111 011110 111110 /;
 
 sub _croak { croak("Couldn't parse '$_[0]' as valid Decimal128") }
+
+sub _erange { croak("Value '$_[0]' is out of range for Decimal128") }
 
 sub _string_to_bid {
     my $s = shift;
@@ -152,19 +167,19 @@ sub _string_to_bid {
     $exp += $dot - length($mant) if $dot >= 0;
 
     # clamping
-    if ( $exp > 6111 && $exp - 6111 <= 34 - length($mant) ) {
-        $mant .= "0"x($exp-6111);
-        $exp = 6111;
+    if ( $exp > AEMAX && $exp - AEMAX <= PLIM - length($mant) ) {
+        $mant .= "0"x($exp-AEMAX);
+        $exp = AEMAX;
     }
 
-    _croak($s) if $exp > 6111 || $exp < -6176;
+    _erange($s) if $exp > AEMAX || $exp < AEMIN;
 
     # Get binary representation of coefficient
     my $coef = Math::BigInt->new($mant)->as_bin;
     $coef =~ s/^0b//;
 
-    # Get binary representation of biased exponent
-    my $biased_exp = unpack( "B*", pack( "n", $exp + 6176 ) );
+    # Get 14-bit binary representation of biased exponent
+    my $biased_exp = unpack( "B*", pack( "n", $exp + BIAS ) );
     substr( $biased_exp, 0, 2, "" );
 
     # Choose representation based on coefficient length
